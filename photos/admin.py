28841c2b9f78ad48b0a1e7388e061b406a8f4bdb
obsoleter <jeremy@obsoleter.com>
2012-11-12 13:47:07 -0500

    Add PublicCollection and PhotoToCollection to admin interface

--- photos/admin.py
+++ photos/admin.py
@@ -1,4 +1,6 @@
-from photos.models import Photo
+from photos.models import (Photo, PublicCollection, PhotoToCollection)
 from django.contrib import admin
 
 admin.site.register(Photo)
+admin.site.register(PublicCollection)
+admin.site.register(PhotoToCollection)
