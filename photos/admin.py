3d9c2133e85249afd137b19281b915cd40d43800
Jeremy A <jeremy@obsoleter.com>
2012-11-13 10:15:02 -0500

    Add list display for photos.  Add collapsible sections on Photo and PublicCollection modeladmins

--- photos/admin.py
+++ photos/admin.py
@@ -1,27 +1,56 @@
 from photos.models import (Photo, Collection, PublicCollection,
                            PhotoToCollection)
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+
 
 class CollectionPhotosInline(admin.TabularInline):
     model = Collection.photos.through
+
 
 class PublicCollectionAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('name', 'description', 'published')}),
         ('Cover photo settings', {'fields': ('cover_image',
-        'cover_image_width', 'cover_image_height')}),
+                                             'cover_image_width',
+                                             'cover_image_height'),
+                                  'classes': ['collapse']}),
         ('Member photo settings', {'fields': ('member_image_width',
-        'member_image_height')}))
+                                              'member_image_height'),
+                                   'classes': ['collapse']}))
     inlines = [CollectionPhotosInline,]
 
+
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('image', 'collection_list')
+    list_display = ('image_display', 'collection_list')
     fieldsets = (
         (None, {'fields': ('image', 'collections')}),
-        ('Dimensions', {'fields': ('width', 'height')}),
-        ('Link to original image', {'fields': ('original',)})
+        ('Dimensions', {'fields': ('width', 'height'),
+                        'classes': ['collapse']}),
+        ('Link to original image', {'fields': ('original',),
+                                    'classes': ['collapse']})
     )
     filter_horizontal = ('collections',)
+
+    def image_display(self, instance):
+        img_template = '<img src="%s" width="100px" height="100px"></img>'
+        return img_template %  (unicode(instance.image.url),)
+
+    image_display.short_description = "Photo"
+    image_display.allow_tags = True
+
+    def collection_list(self, instance):
+        link_template = '<a href="%s">%s</a>'
+        return ', '.join(
+            link_template % (
+                reverse('admin:photos_publiccollection_change',
+                    args=(c['id'],)
+                ),
+                c['name']
+            ) for c in instance.collections.values())
+    
+    collection_list.short_description = "Collections"
+    collection_list.allow_tags = True
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(PublicCollection, PublicCollectionAdmin)
