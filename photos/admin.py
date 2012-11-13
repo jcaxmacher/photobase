76d036763329bc8bf02fcd9906bb54e49f7779bc
Jeremy A <jeremy@obsoleter.com>
2012-11-12 21:53:31 -0500

    Clean up fieldsets in photoadmin adn publiccollectionadmin

--- photos/admin.py
+++ photos/admin.py
@@ -2,16 +2,24 @@ from photos.models import (Photo, Collection, PublicCollection,
                            PhotoToCollection)
 from django.contrib import admin
 
-class CollectionInline(admin.TabularInline):
+class CollectionPhotosInline(admin.TabularInline):
     model = Collection.photos.through
 
 class PublicCollectionAdmin(admin.ModelAdmin):
-    inlines = [CollectionInline,]
+    fieldsets = (
+        (None, {'fields': ('name', 'description', 'published')}),
+        ('Cover photo settings', {'fields': ('cover_image',
+        'cover_image_width', 'cover_image_height')}),
+        ('Member photo settings', {'fields': ('member_image_width',
+        'member_image_height')}))
+    inlines = [CollectionPhotosInline,]
 
 class PhotoAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': ('image', 'width', 'height', 'original',
-        'collections')}),)
+        (None, {'fields': ('image', 'collections')}),
+        ('Dimensions', {'fields': ('width', 'height')}),
+        ('Link to original image', {'fields': ('original',)})
+    )
     filter_horizontal = ('collections',)
 
 admin.site.register(Photo, PhotoAdmin)
